3198bfc1191b15816e64fec7a5fc8d426e7de84c
TheBeachMaster <otienoken@outlook.com>
2016-11-05 16:15:28 -0700

    Cleaned up dbConnect

--- admin/dbconnect.php
+++ admin/dbconnect.php
@@ -1,29 +1,5 @@
 <?php
 
- 
- //error_reporting( ~E_DEPRECATED & ~E_NOTICE );
- /*
- define('DBHOST', 'localhost');
- define('DBUSER', 'root');
- define('DBPASS', 'password');
- define('DBNAME', 'disastermanagement');
- */
-/*
-$dbhost = "localhost";
-$user = "root";
-$pass = "password";
-$db = "disastermanagement";
-
- $conn = mysql_connect($dbhost,$user,$pass);
- $dbcon = mysql_select_db($db);
- 
- if ( !$conn ) {
-  die("Connection failed : " . mysql_error());
- }
- 
- if ( !$dbcon ) {
-  die("Database Connection failed : " . mysql_error());
- }*/
  $i_link = mysqli_connect("localhost","root" ,"password","disastermanagement");
 
 /* check connection */
